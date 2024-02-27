7f089cc26c3dfaaa043371d0e9bfc4710b3ff5fc
mukanjy0 <mukanjyo29@protonmail.com>
2024-02-27 03:34:56 -0500

    added solution to Bacon's Cipher from ACM ICPC 2007–2008

--- timus1590.cpp
+++ timus1590.cpp
@@ -1,3 +1,150 @@
-//
-// Created by iansg on 2/26/2024.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+class suffix_tree {
+    class Node {
+        int l {}; // inclusive left bound
+        int r {}; // exclusive right bound
+        int j {};
+        Node* par {};
+        Node* link {};
+        map<char,Node*> children;
+        friend suffix_tree;
+    public:
+        Node() : j(-1), link(this) {}
+        Node(int l, int r, Node* par = nullptr, int j = -1) : l(l), r(r), par(par), j(j) {}
+        [[nodiscard]] int length() const { return r-l; }
+        ll substr_cnt() {
+            ll p = (ll)length();
+//            if (r == 17) cout << p << '\n';
+//            cout << l << ' ' << r << ' ' << p << '\n';
+            for (auto [c, v] : children)
+                p += v->substr_cnt();
+            return p;
+        }
+    };
+
+    void _clear(Node* u) {
+        for (auto [c, v] : u->children) _clear(v);
+        delete u;
+    }
+
+    string s;
+    int m;
+    Node* root {};
+public:
+    explicit suffix_tree(string s) : s(s) {
+        m = (int)s.size();
+        build();
+    }
+
+    ~suffix_tree() { clear(); }
+
+    void build() {
+        if (s.empty()) return;
+
+        root = new Node(1, 1);
+        root->link = root;
+
+        Node* v = new Node(0,m,root,0);
+        root->children[s[0]] = v; // I_1
+
+        int j = 1; // j* explicit extension start
+        int k = 0;
+        int pos = 1;
+        bool implicit = true;
+        Node* u = root;
+        for (int i = 0; i < m-1; ++i) {
+            Node* prev {};
+
+            for (; j <= i+1; ++j) {
+                if (!implicit) {
+                    if (pos != u->r) {
+                        k -= u->length();
+                        u = u->par;
+                    }
+                    k = max(0,k-1);
+                    u = u->link;
+                }
+                else implicit = false;
+
+                int len = i+1-j;
+                while (k < len) {
+                    u = u->children[s[j+k]];
+                    k += u->length();
+                }
+                pos = u->r - (k-len);
+                if (pos==u->r) {
+                    if (prev) {
+                        prev->link = u;
+                        prev = nullptr;
+                    }
+
+                    if (u->children.count(s[i+1])) { implicit = true; break; }
+
+                    v = new Node(i+1,m,u,j);
+                    u->children[s[i+1]] = v;
+                }
+                else {
+                    if (s[pos] == s[i+1]) { implicit = true; break; }
+                    k -= u->length();
+
+                    Node* p = new Node(u->l, pos, u->par);
+                    u->par->children[s[u->l]] = p;
+                    p->children[s[pos]] = u;
+                    u->par = p;
+                    u->l = pos;
+                    v = new Node(i+1,m,p,j);
+                    p->children[s[i+1]] = v;
+
+                    if (prev) prev->link = p;
+                    prev = p;
+
+                    u = p->par;
+                    pos = u->r;
+                }
+            }
+        }
+    }
+
+    ll substr_cnt() {
+        if (root) return root->substr_cnt();
+        return 0;
+    }
+
+    bool search(const string& p) {
+        if (root == nullptr) return false;
+        Node* u = root;
+        int i = 0, n = (int)p.size();
+        while (i < n) {
+            if (!u->children.count(p[i])) {
+                cout << i << '\n';
+                return false;
+            }
+            u = u->children[p[i]];
+            for (int j = u->l; j < u->r; ++j,++i) {
+                if (i==n) return true;
+                if (s[j] != p[i]) {
+                    cout << i << '\n';
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void clear() {
+        _clear(root);
+        root = nullptr;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    string s; cin >> s;
+    suffix_tree sf (s);
+    cout << sf.substr_cnt() << '\n';
+    return 0;
+}
